6936027d862292676408b3801a165f3b7f26db30
Corbin Coleman <corbincol@hotmail.com>
2017-06-20 17:02:00 -0700

    Add new function prototypes. Also, add organization comments detailing what files functions are from and briefly what they are for.

--- maze.h
+++ maze.h
@@ -63,22 +63,64 @@ typedef struct keys
 	int left;
 } keys;
 
+/**
+ * File - init_instance.c
+ * Initializes instances for windo and renderer
+ **/
 int init_instance(SDL_Instance *);
-void fill_screen_by_line(SDL_Instance);
+
+/**
+ * File - event_handlers.c
+ * Functions keep track of different keyboard events
+ **/
 int keyboard_events(keys *);
-char **create_map(char *, double_s *, int_s *);
-void draw_walls(char **, double_s, SDL_Instance, double_s, double_s);
-void choose_color(SDL_Instance, char **, int_s, int);
-void rotate(double_s *, double_s *, int);
-void movement(keys, double_s *, double_s *, double_s *, char **);
-void draw_background(SDL_Instance);
-void print_win(void);
-int check_win(double_s, int_s, int *);
-void plot_grid_points(char **, double_s *, int_s *, size_t, size_t, char *);
 void check_key_release_events(SDL_Event, keys *);
 int check_key_press_events(SDL_Event, keys *);
+
+/**
+ * File - create_maze.c
+ * Functions fill out the 2D array "map", which represents play space
+ **/
+char **create_map(char *, double_s *, int_s *, size_t *);
+void plot_grid_points(char **, double_s *, int_s *, size_t, size_t, char *);
+size_t get_line_count(char *);
+size_t get_char_count(char *);
+
+/**
+ * File - draw.c
+ * Functions necessary to draw the sky, floor, and walls of the maze
+ **/
+void draw(SDL_Instance, char **, double_s, double_s, double_s);
+void draw_walls(char **, double_s, SDL_Instance, double_s, double_s);
+void choose_color(SDL_Instance, char **, int_s, int);
+void draw_background(SDL_Instance);
+
+/**
+ * File - movement.c
+ * Functions rotate/move player
+ **/
+void rotate(double_s *, double_s *, int);
+void movement(keys, double_s *, double_s *, double_s *, char **);
+
+/**
+ * File - win.c
+ * Functions to handle the win condition of the game
+ **/
+void print_win(void);
+int check_win(double_s, int_s, int *);
+
+/**
+ * File - dist_checks.c
+ * Check distance to walls and direction the ray being cast is travelling in
+ **/
 double get_wall_dist(char **, double_s *, int_s *, int_s *, double_s *, int *,
 		     double_s *, double_s *);
 void check_ray_dir(int_s *, double_s *, double_s, int_s, double_s, double_s);
-void draw(SDL_Instance, char **, double_s, double_s, double_s);
+
+/**
+ * File free_stuff.c
+ * Frees memory allocated during run time
+ **/
+void free_memory(SDL_Instance, char **, size_t);
+void free_map(char **, size_t);
 #endif
