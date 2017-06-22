684d2972c6dee282994bef128f2a928aadb2502f
Corbin Coleman <corbincol@hotmail.com>
2017-06-21 17:43:01 -0700

    Start implementing a way of holding multiple maps at once

--- main_maze.c
+++ main_maze.c
@@ -9,23 +9,35 @@
 int main(int argc, char *argv[])
 {
 	SDL_Instance instance;
-	char **map;
+	level *levels;
+	level cur_level = {NULL, 0};
+	/*char **map;*/
 	size_t map_h = 0;
-	int win_value = 0;
+	int i, j, win_value = 0;
 	int_s win = {0, 0};
 	double_s play = {2, 2};
 	double_s dir = {-1, 0};
 	double_s plane = {0, 0.5};
 	keys key_press = {0, 0, 0, 0};
 
+	j = 0;
 	if (argc < 2)
 		return (1);
-	map = create_map(argv[1], &play, &win, &map_h);
-	if (map == NULL)
+	levels = malloc(sizeof(level) * (argc - 1));
+	if (levels == NULL)
+		return (1);
+	for (i = 1; i < argc; i++, j++)
+	{
+		cur_level.map = create_map(argv[i], &play, &win, &map_h);
+		cur_level.height = map_h;
+		levels[j] = cur_level;
+	}
+/*	map = create_map(argv[1], &play, &win, &map_h);*/
+/*	if (levels == NULL)
 	{
 		printf("Unable to create map\n");
 		return (1);
-	}
+		}*/
 	if (init_instance(&instance) != 0)
 	{
 		printf("Unable to initialize SDL_Instance\n");
@@ -35,13 +47,16 @@ int main(int argc, char *argv[])
 	{
 		if (keyboard_events(&key_press))
 			break;
-		movement(key_press, &plane, &dir, &play, map);
+/*		movement(key_press, &plane, &dir, &play, map);*/
+		movement(key_press, &plane, &dir, &play, levels[0].map);
 		if (check_win(play, win, &win_value))
 			break;
-		draw(instance, map, play, dir, plane);
+/*		draw(instance, map, play, dir, plane);*/
+		draw(instance, levels[0].map, play, dir, plane);
 	}
 	if (win_value)
 		print_win();
-	free_memory(instance, map, map_h);
+/*	free_memory(instance, map, map_h);*/
+	free_memory(instance, levels[0].map, levels[0].height);
 	return (0);
 }
