9e0382d0ac0d552daad0aba51f065aaf1e8ef034
Corbin Coleman <corbincol@hotmail.com>
2017-06-26 12:37:32 -0700

    Use new level struct and an array of level structs to allow for multiple levels played in same game

--- main_maze.c
+++ main_maze.c
@@ -10,53 +10,41 @@ int main(int argc, char *argv[])
 {
 	SDL_Instance instance;
 	level *levels;
-	level cur_level = {NULL, 0};
-	/*char **map;*/
-	size_t map_h = 0;
-	int i, j, win_value = 0;
-	int_s win = {0, 0};
-	double_s play = {2, 2};
-	double_s dir = {-1, 0};
-	double_s plane = {0, 0.5};
+	int lvl, win_value, num_of_levels;
 	keys key_press = {0, 0, 0, 0};
 
-	j = 0;
-	if (argc < 2)
+	lvl = win_value = 0;
+	num_of_levels = argc;
+	if (num_of_levels < 2)
 		return (1);
-	levels = malloc(sizeof(level) * (argc - 1));
+	levels = build_world_from_args(argc, argv);
 	if (levels == NULL)
 		return (1);
-	for (i = 1; i < argc; i++, j++)
-	{
-		cur_level.map = create_map(argv[i], &play, &win, &map_h);
-		cur_level.height = map_h;
-		levels[j] = cur_level;
-	}
-/*	map = create_map(argv[1], &play, &win, &map_h);*/
-/*	if (levels == NULL)
-	{
-		printf("Unable to create map\n");
-		return (1);
-		}*/
 	if (init_instance(&instance) != 0)
-	{
-		printf("Unable to initialize SDL_Instance\n");
 		return (1);
-	}
+	lvl = 0;
 	while (1)
 	{
 		if (keyboard_events(&key_press))
+		{
+			free_memory(instance, levels[lvl].map, levels[lvl].height);
 			break;
-/*		movement(key_press, &plane, &dir, &play, map);*/
-		movement(key_press, &plane, &dir, &play, levels[0].map);
-		if (check_win(play, win, &win_value))
-			break;
-/*		draw(instance, map, play, dir, plane);*/
-		draw(instance, levels[0].map, play, dir, plane);
+		}
+		movement(key_press, &levels[lvl].plane, &levels[lvl].dir, &levels[lvl].play,
+			 levels[lvl].map);
+		if (check_win(levels[lvl].play, levels[lvl].win, &win_value))
+		{
+			free_map(levels[lvl].map, levels[lvl].height);
+			lvl++;
+			if (lvl == argc - 1)
+				break;
+			win_value = 0;
+		}
+		draw(instance, levels[lvl].map, levels[lvl].play, levels[lvl].dir,
+		     levels[lvl].plane);
 	}
+	close_SDL(instance);
 	if (win_value)
 		print_win();
-/*	free_memory(instance, map, map_h);*/
-	free_memory(instance, levels[0].map, levels[0].height);
 	return (0);
 }
